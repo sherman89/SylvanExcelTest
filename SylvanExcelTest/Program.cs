8bcf76e9a0805caa695287e4f00acaa571c50bf0
MarkPflug <MarkPflug@users.noreply.github.com>
2024-06-12 21:53:48 -0700

    Updated validation example

--- SylvanExcelTest/Program.cs
+++ SylvanExcelTest/Program.cs
@@ -1,215 +1,186 @@
 ﻿using Sylvan.Data;
 using Sylvan.Data.Excel;
-using System.Collections.Immutable;
-using System.Collections.ObjectModel;
 using System.Data.Common;
-using System.Diagnostics;
 
-namespace SylvanExcelTest
+class Program
 {
-    internal class Program
+    static int Main()
     {
-        static async Task<int> Main(string[] args)
+        const string UserNameEn = "Users";
+        var userSchemaEn = Schema.Parse("Id:int,First name:string,Last name:string,Age:int");
+        const string UserNameFi = "Käyttäjät";
+        var userSchemaFi = Schema.Parse("Id:int,Etunimi:string,Sukunimi:string,Age:int");
+        const string EmailNameEn = "E-mails";
+        var emailSchemaEn = Schema.Parse("Id:int,UserId:int,E-mail address:string");
+        const string EmailNameFi = "Sähköpostit";
+        var emailSchemaFi = Schema.Parse("Id:int,KäyttäjäId:int,Sähköposti:string");
+
+        var schema = new ExcelSchema();
+        schema.Add(UserNameEn, true, userSchemaEn);
+        schema.Add(EmailNameEn, true, emailSchemaEn);
+        schema.Add(UserNameFi, true, userSchemaFi);
+        schema.Add(EmailNameFi, true, emailSchemaFi);
+
+        var opts = new ExcelDataReaderOptions { Schema = schema };
+
+        // this new file has some extra validation examples
+        using var edr = ExcelDataReader.Create("TestData/Data_ENG2.xlsx", opts);
+            
+        do
         {
-            // TODO: Maybe would be nice to use this, but how to support multiple worksheets? Have to use 2 readers
-            // var userWorksheetSchema = Schema.Parse("Id:string,FirstName:string,LastName:string,Age:string");
-            // var emailWorksheetSchema = Schema.Parse("Id:string,UserId:string,Email:string");
-            // var excelSchema = new ExcelSchema(hasHeaders: true, userWorksheetSchema);
-
-            var schema = new MyExcelSchema();
-
-            try
+            Validator validator = null;
+            switch (edr.WorksheetName)
             {
-                // Get directory path to exe so we can read test xlsx files that are copied to output dir
-                var procDirPath = Path.GetDirectoryName(Environment.ProcessPath);
-
-                await using var edr = await ExcelDataReader.CreateAsync(
-                    @$"{procDirPath}\TestData\Data_FIN.xlsx",
-                    new ExcelDataReaderOptions
-                    {
-                        Schema = schema
-                    });
-
-                
-                // TODO: How to get this called while getting records / reading?
-                // TODO: Why edr works but not this when in GetRecordsAsync?
-                var reader = edr.Validate(context =>
-                {
-                    // I don't know what I'm doing here :)
-                    var errors = context.GetErrors().ToList();
-                    Console.WriteLine($"Ordinals with errors: {string.Join(", ", errors)}");
-                    return errors.Count == 0;
-                });
-
-                var result = new ResultRecord();
-
-                do
-                {
-                    if (edr.WorksheetName == null)
-                    {
-                        continue;
-                    }
-
-                    var sheetName = schema.MapToInternalWorksheetName(edr.WorksheetName);
-
-                    switch (sheetName)
-                    {
-                        case MyExcelSchema.UsersWorksheet:
-                        {
-                            var rowNumber = edr.RowNumber;
-                            var columns = edr.GetColumnSchema();
-                            result.Users = await edr.GetRecordsAsync<UserRecord>().ToArrayAsync();
-                            ValidateUsers(result.Users, columns, rowNumber);
-                            break;
-                        }
-                        case MyExcelSchema.EmailsWorksheet:
-                            result.Emails = await edr.GetRecordsAsync<EmailRecord>().ToArrayAsync();
-                            break;
-                    }
-                } while (await edr.NextResultAsync());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return -1;
+                case UserNameEn:
+                    validator = new UserValidator(edr, userSchemaEn);
+                    break;
+                case UserNameFi:
+                    validator = new UserValidator(edr, userSchemaFi);
+                    break;
+                case EmailNameEn:
+                    validator = new EmailValidator(edr, emailSchemaEn);
+                    break;
+                case EmailNameFi:
+                    validator = new EmailValidator(edr, emailSchemaFi);
+                    break;
+                default:
+                    Console.WriteLine("Unknown worksheet " + edr.WorksheetName);
+                    break;
             }
 
-            return 0;
-        }
-
-        private static void ValidateUsers(UserRecord[] data, ReadOnlyCollection<DbColumn> columns, int firstRowNumber)
-        {
-            // TODO: A lot of logic just to validate one column... doing this for all would not look pretty? how to simplify?
-
-            var emptyFirstNames = data
-                .Select((usr, index) => new { User = usr, index })
-                .Where(grp => string.IsNullOrWhiteSpace(grp.User.FirstName))
-                .ToList();
-
-            foreach (var user in emptyFirstNames)
+            if (validator != null)
             {
-                var firstNameCol = columns.First(c => c.ColumnName == nameof(UserRecord.FirstName));
-
-                Debug.Assert(firstNameCol.ColumnOrdinal != null, "Schema always sets Ordinal, shouldn't be null");
-                var columnPosition = ExcelHelpers.GetExcelColumnName(firstNameCol.ColumnOrdinal.Value + 1);
-
-                Console.WriteLine($"Invalid value at position {columnPosition}:{firstRowNumber + user.index + 1}. " +
-                                  $"Column: {firstNameCol.BaseColumnName}, Worksheet: {firstNameCol.BaseTableName}");
-            }
-        }
-    }
-
-    public record ResultRecord
-    {
-        public UserRecord[]? Users { get; set; }
-
-        public EmailRecord[]? Emails { get; set; }
-    }
-
-    public record UserRecord
-    {
-        public string? Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Age { get; set; }
-    }
-
-    public record EmailRecord
-    {
-        public string? Id { get; set; }
-        public string? UserId { get; set; }
-        public string? Email { get; set; }
-    }
-
-    public sealed class MyExcelSchema : ExcelSchemaProvider
-    {
-        // _columnMapping: I could also map by name e.g. "{UsersWorksheet}.Id" but maybe better by ordinal?
-
-        private readonly ImmutableDictionary<string, ExcelColumn> _columnMapping =
-            ImmutableDictionary.CreateRange(StringComparer.OrdinalIgnoreCase,
-                new KeyValuePair<string, ExcelColumn>[]
+                var reader = edr.Validate(validator.Validate);
+                while (reader.Read())
                 {
-                    // Users
-                    new($"{UsersWorksheet}.0", UserIdCol),
-                    new($"{UsersWorksheet}.1", UserFirstNameCol),
-                    new($"{UsersWorksheet}.2", UserLastNameCol),
-                    new($"{UsersWorksheet}.3", UserAgeCol),
-                    // Emails
-                    new($"{EmailsWorksheet}.0", EmailIdCol),
-                    new($"{EmailsWorksheet}.1", EmailUserIdCol),
-                    new($"{EmailsWorksheet}.2", EmailCol)
-                });
-
-        private readonly ImmutableDictionary<string, string> _worksheetMapping =
-            ImmutableDictionary.CreateRange(StringComparer.OrdinalIgnoreCase,
-                new KeyValuePair<string, string>[]
-                {
-                    new("Users", UsersWorksheet),
-                    new("Käyttäjät", UsersWorksheet),
-                    new("E-mails", EmailsWorksheet),
-                    new("Sähköpostit", EmailsWorksheet)
-                });
-
-        public const string UsersWorksheet = "Users";
-        private static readonly ExcelColumn UserIdCol = new (nameof(UserRecord.Id), typeof(string), 0, UsersWorksheet);
-        private static readonly ExcelColumn UserFirstNameCol = new (nameof(UserRecord.FirstName), typeof(string), 1, UsersWorksheet);
-        private static readonly ExcelColumn UserLastNameCol = new (nameof(UserRecord.LastName), typeof(string), 2, UsersWorksheet);
-        private static readonly ExcelColumn UserAgeCol = new (nameof(UserRecord.Age), typeof(string), 3, UsersWorksheet);
-
-        public const string EmailsWorksheet = "E-mail";
-        private static readonly ExcelColumn EmailIdCol = new (nameof(EmailRecord.Id), typeof(string), 0, EmailsWorksheet);
-        private static readonly ExcelColumn EmailUserIdCol = new (nameof(EmailRecord.UserId), typeof(string), 1, EmailsWorksheet);
-        private static readonly ExcelColumn EmailCol = new (nameof(EmailRecord.Email), typeof(string), 2, EmailsWorksheet);
-
-        public override DbColumn? GetColumn(string sheetName, string? name, int ordinal)
-        {
-            var mappedWorksheetName = MapToInternalWorksheetName(sheetName);
-            if (mappedWorksheetName == null)
-            {
-                return null;
+                    // only valid rows are yielded in the loop
+                    Console.WriteLine($"Valid {edr.WorksheetName} {edr.RowNumber}");
+                }
             }
 
-            if (string.IsNullOrWhiteSpace(name))
+        } while (edr.NextResult());
+
+        return 0;
+    }
+
+    abstract class Validator
+    {
+        public bool Validate(DataValidationContext context)
+        {
+            ExcelDataReader edr = (ExcelDataReader) context.DataReader;
+            var valid = LogSchemaErrors(context, edr);
+            valid &= ValidateCustom(context, edr);
+            return valid;
+        }
+
+        protected abstract bool ValidateCustom(DataValidationContext context, ExcelDataReader edr);
+
+        protected void LogError(ExcelDataReader dr, int ord)
+        {
+            var name = dr.GetName(ord);
+            var value = dr.GetString(ord); // always safe to GetString
+            var type = dr.GetDataTypeName(ord);
+            Console.WriteLine($"Invalid {name} at row {dr.RowNumber} col {ord} value '{value}'.");
+        }
+
+        protected bool LogSchemaErrors(DataValidationContext context, ExcelDataReader dr)
+        {
+            var valid = true;
+            foreach (var ord in context.GetErrors())
             {
-                var colPosition = ExcelHelpers.GetExcelColumnName(ordinal + 1);
-                // TODO: Can I get row id here somehow in-case header is not first row?
-                throw new ExcelReadException($"Column in worksheet \"{sheetName}\" at position \"{colPosition}\" has an empty header.", ordinal)
+                LogError(dr, ord);
+                valid = false;
+            }
+            return valid;
+        }
+    }
+
+    class UserValidator : Validator
+    {
+        int idOrd, fnOrd, lnOrd, ageOrd;
+        
+        HashSet<int> seenIds = new HashSet<int>();
+
+        public UserValidator(DbDataReader reader, Schema schema)
+        {
+            idOrd = reader.GetOrdinal(schema[0].ColumnName);
+            fnOrd = reader.GetOrdinal(schema[1].ColumnName);
+            lnOrd = reader.GetOrdinal(schema[2].ColumnName);
+            ageOrd = reader.GetOrdinal(schema[3].ColumnName);
+        }
+
+        protected override bool ValidateCustom(DataValidationContext context, ExcelDataReader dr)
+        {
+            var valid = true;
+
+            if (context.IsValid(idOrd)) 
+            {
+                var id = context.GetValue<int>(idOrd);
+                if (!seenIds.Add(id))
                 {
-                    WorksheetName = sheetName,
-                    ColumnName = name
-                };
+                    // already seen it
+                    Console.WriteLine($"Duplicate Id at row {dr.RowNumber} col {idOrd} value '{id}'.");
+                }
             }
 
-            return CollectionExtensions.GetValueOrDefault(_columnMapping, $"{mappedWorksheetName}.{ordinal}");
-        }
+            var fn = dr.GetString(fnOrd);
+            if (string.IsNullOrEmpty(fn))
+            {
+                LogError(dr, fnOrd);
+                valid = false;
+            }
 
-        public override bool HasHeaders(string sheetName)
-        {
-            return true;
-        }
+            var ln = dr.GetString(lnOrd);
+            if (string.IsNullOrEmpty(ln))
+            {
+                LogError(dr, lnOrd);
+                valid = false;
+            }
 
-        public string? MapToInternalWorksheetName(string sheetName)
-        {
-            return CollectionExtensions.GetValueOrDefault(_worksheetMapping, sheetName.Trim());
+            var age = context.GetValue<int>(ageOrd);
+            if (age < 0)
+            {
+                LogError(dr, ageOrd);
+                valid = false;
+            }
+
+            return valid;
         }
     }
 
-    public class ExcelColumn : DbColumn
+    class EmailValidator : Validator
     {
-        public ExcelColumn(string name, Type type, int ordinal, string tableName, bool allowNull = false)
+        int idOrd, userOrd, emailOrd;
+
+        public EmailValidator(DbDataReader reader, Schema schema)
         {
-            ColumnName = name;
-            DataType = type;
-            AllowDBNull = allowNull;
-            ColumnOrdinal = ordinal;
-            BaseTableName = tableName;
+            idOrd = reader.GetOrdinal(schema[0].ColumnName);
+            userOrd = reader.GetOrdinal(schema[1].ColumnName);
+            emailOrd = reader.GetOrdinal(schema[2].ColumnName);
+        }
+
+        protected override bool ValidateCustom(DataValidationContext context, ExcelDataReader edr)
+        {
+            var valid = true;
+            var email = edr.GetString(emailOrd);
+            if (!email.Contains("@"))
+            {
+                LogError(edr, emailOrd);
+                valid = false;
+            }
+
+            return valid;
         }
     }
 
-    public class ExcelReadException(string? message, int ordinal) : Exception(message)
+}
+
+static class Ex
+{
+    // I will add an efficient version of this API to Sylvan.Data
+    public static bool IsValid(this DataValidationContext c, int ord)
     {
-        public string? WorksheetName { get; set; }
-        public string? ColumnName { get; set; }
-        public int Ordinal { get; set; } = ordinal;
+        // this is a bit slower than it needs to be
+        return c.GetErrors().Contains(ord) == false;
     }
 }
